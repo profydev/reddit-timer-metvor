b1cb58543b87b2c1625833b01532aa5dabaf800f
Mette Vorraa <mettevorraa@gmail.com>
2022-01-03 16:24:46 +0000

    create Header component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import { Link } from "react-router-dom";
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import GlobalStyle from './GlobalStyle';
+import { Normalize } from 'styled-normalize';
+import Header from './Header';
+import theme from './theme';
 
 export default function App() {
   return (
-    <div>
-      <h1>App Placeholder</h1>
-      <nav
-        style={{
-          borderBottom: "solid 1px",
-          paddingBottom: "1rem"
-        }}
-      >
-        <Link to="/somewhere">/Somewhere</Link> 
-        |
-        {" "}
-        <Link to="/search">/Search</Link>
-      </nav>
-    </div>
+    <ThemeProvider theme={theme}>
+    <Router>
+      <Normalize />
+      <GlobalStyle />
+      <Header />
+      <Switch>
+        <Route path="/search">Search</Route>
+        <Route path="/">Home</Route>
+      </Switch>
+    </Router>
+  </ThemeProvider>
   );
 }
